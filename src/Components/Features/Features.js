2141c4531bc95ab17b63b883b5730dbf494236ab
Ayushee <ashrivastava0812@gmail.com>
2022-01-05 23:03:26 +0530

    features section added and kuchi bhi section css

--- src/Components/Features/Features.js
+++ src/Components/Features/Features.js
@@ -5,23 +5,23 @@ import { BsFillShareFill } from "react-icons/bs";
 function Features() {
   return (
     <div>
-      <h1 className="text-center">Features</h1>
-      <div className="container" style={{ marginTop: "1rem" }}>
-        <div className="row">
-          <div className="col-sm-4">
-            <div style={{ display: "flex", flexDirection: "row" }}>
+      <h1 className="text-center">FEATURES</h1>
+      <div className="container p-3" style={{ marginTop: "1rem" }}>
+        <div className="row p-5 ">
+          <div className="col-sm-4 mt-2">
+            <div style={{ display: "flex", flexDirection: "row" ,'justifyContent':'center'}}>
               <BsMusicNoteList size={40} />
               <h2>Manage Songs</h2>
             </div>
           </div>
-          <div className="col-sm-4">
-            <div style={{ display: "flex", flexDirection: "row" }}>
+          <div className="col-sm-4 mt-2">
+            <div style={{ display: "flex", flexDirection: "row" ,'justifyContent':'center' }}>
               <BsMusicPlayerFill size={40} />
               <h2>Create Playlist</h2>
             </div>
           </div>
-          <div className="col-sm-4">
-            <div style={{ display: "flex", flexDirection: "row" }}>
+          <div className="col-sm-4 mt-2">
+            <div style={{ display: "flex", flexDirection: "row" ,'justifyContent':'center'}}>
               <BsFillShareFill size={40} />
               <h2>Share</h2>
             </div>
--- src/Components/KuchBhi.js
+++ src/Components/KuchBhi.js
@@ -5,12 +5,12 @@ function KuchBhi() {
     <div style={{ height: "auto" }}>
       <div className="container mt-5">
         <div className="row">
-          <div className="col-lg-6 " style={{ height: "300px" }}>
-            <div className="text-center img-fluid" className="">
+          <div className="col-lg-6 " style={{ height: "auto" }}>
+            <div className="text-center " className="">
               <img src={img} alt="info_img" classNameName="" height={300} />
             </div>
           </div>
-          <div className="col-lg-6 mt-5" style={{ height: "300px" }}>
+          <div className="col-lg-6 mt-5" style={{ height: "auto" }}>
             <p className=" text-center">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               reprehenderit in voluptate velit esse cillum dolore eu fugiat
